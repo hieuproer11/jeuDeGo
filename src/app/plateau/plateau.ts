3754aedc5d9486dcbc0f061676dd9132e21635c2
HIEU TRAN <q.tran@lyon.ort.asso.fr>
2025-12-03 11:55:28 +0100

    mise a jour du plateau, onIntersectionClick()

--- src/app/plateau/plateau.ts
+++ src/app/plateau/plateau.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class Plateau {
   size = 9;
   board: number[][] = [];
+  overlayBoard: number[][] = [];
 
   constructor(private router: Router) {
     this.reset();
@@ -18,6 +19,7 @@ export class Plateau {
 
   reset() {
     this.board = Array(this.size).fill(null).map(() => Array(this.size).fill(0));
+    this.overlayBoard = Array(this.size - 1).fill(null).map(() => Array(this.size - 1).fill(0));
   }
 
   makeAiMove() {
@@ -25,9 +27,9 @@ export class Plateau {
     console.log('AI move');
   }
 
-  onCellClick(row: number, col: number) {
-    console.log(`Clicked: ${row}, ${col}`);
-    // Logique de placement de pierre
+  onIntersectionClick(row: number, col: number) {
+    console.log(`Intersection clicked: ${row}, ${col}`);
+    // Logique de placement de pierre aux intersections
   }
 
   goHome() {
